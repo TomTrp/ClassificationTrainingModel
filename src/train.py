8a33e6a369c792b35ae0877f30799461212008f7
thiraphat <thiraphat.schai@gmail.com>
2025-11-30 19:13:31 +0700

    feat: add random forest training_method

--- src/train.py
+++ src/train.py
@@ -1,10 +1,12 @@
 import kagglehub
 import pandas as pd
+import pickle
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 import os
 
@@ -17,30 +19,32 @@ def getFruitsDataSet():
     print(csv_path)
     return csv_path
 
-def createDecisionTreePipeline(preprocessor):
-    dt_pipeline = Pipeline(steps=[
+# Pipeline
+def createPipeline(preprocessor, x_train, y_train, train_method = ""):
+    if train_method == "decision_tree":
+        classifier = DecisionTreeClassifier(random_state=42)
+    elif train_method == "random_forest":
+        classifier = RandomForestClassifier(n_estimators=100, random_state=42)
+    else:
+        raise ValueError(f"train_method '{train_method}' not supported train_methpd")
+    pipeline = Pipeline(steps=[
         ("preprocessor", preprocessor),
-        ("classifier", DecisionTreeClassifier(random_state=42))
+        ("classifier", classifier)
     ])
-    return dt_pipeline
-
-
+    return pipeline
 
 # Load dataset
 csv_path = getFruitsDataSet()
 df = pd.read_csv(csv_path)
 print(df)
 
-
 # Extract features and target
-X = df.drop("fruit_name", axis=1)
+x = df.drop("fruit_name", axis=1)
 y = df["fruit_name"]
-
 
 # Define categorical and numeric columns
 categorical_features = ["shape", "color", "taste"]
 numeric_features = ["size (cm)", "weight (g)", "avg_price (₹)"]
-
 
 # Create ColumnTransformer for encode categorical
 preprocessor = ColumnTransformer(
@@ -50,14 +54,23 @@ preprocessor = ColumnTransformer(
     remainder="passthrough"
 )
 
-
 # Split Train/Test
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42)
 
-dt_pipeline = createDecisionTreePipeline(preprocessor)
+dt_pipeline = createPipeline(preprocessor, x_train, y_train, "decision_tree")
+rf_pipeline = createPipeline(preprocessor, x_train, y_train, "random_forest")
 
-dt_pipeline.fit(X_train, y_train)
-y_pred_dt = dt_pipeline.predict(X_test)
+# Train
+dt_pipeline.fit(x_train, y_train)
+rf_pipeline.fit(x_train, y_train)
 
+# Test
+y_pred_dt = dt_pipeline.predict(x_test)
+y_pred_rf = rf_pipeline.predict(x_test)
+
+# Evaluate result
 print("Decision Tree Accuracy:", accuracy_score(y_test, y_pred_dt))
 print(classification_report(y_test, y_pred_dt))
+
+print("Random Forest Accuracy:", accuracy_score(y_test, y_pred_rf))
+print(classification_report(y_test, y_pred_rf))
